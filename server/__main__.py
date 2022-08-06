1208c9562668be3406d4365aec31672d63c905ef
猴子 <a102009102009@gmail.com>
2022-08-06 08:27:29 +0800

    fix

--- server/__main__.py
+++ server/__main__.py
@@ -2,6 +2,6 @@
 
 if __name__ == "__main__":
     # main()
-    from chatbridge.core import ChatBridgeServer, Address
+    from server.core import ChatBridgeServer, Address
 
     ChatBridgeServer("test", Address("localhost", 9999)).start()
--- server/core/client.py
+++ server/core/client.py
@@ -23,18 +23,34 @@ class ChatBridgeClient(BaseChatBridge):
     def set_state(self, state: ClientState) -> None:
         self.__state = state
 
-    async def __connect(self):
+    def __connect(self):
         self.set_state(ClientState.CONNECTING)
         self._sock.connect(self.server_address)
 
     async def _connect_and_login(self):
-        self.__connect()
-        self.set_state(ClientState.ONLINE)
-        ...
+        self.__connect(self.server_address)
+        await self.send_json(self._sock, {"name": "", "password": ""})
+        # self.set_state(ClientState.ONLINE)
 
-    async def close(self):
+    async def get_requests(self):
+        while True:
+            data = await self.receive_data(self._sock)
+
+            if not data:
+                self.close()
+                break
+
+            if type(data) != dict:
+                continue
+
+            data["type"]
+
+            print(data)
+        self.close()
+
+    def close(self):
         self._sock.close()
-        self.set_state(ClientState.ONLINE)
+        self.set_state(ClientState.DISCONNECTED)
 
 
 # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
