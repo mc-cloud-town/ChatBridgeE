f65b627728fa0e0bcd57026689a26574b41dfdc8
猴子 <a102009102009@gmail.com>
2022-11-11 22:40:30 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,20 +1,68 @@
-from typing import Callable, Union
+# while True:
+#     try:
+#         print(next(t))
+#     except StopIteration:
+#         break
 
 
-def listen(arg: Union[str, Callable] = None, *args, **kwargs):
-    name = arg
-
-    def decorator(func: Callable):
-        print(func, name or func.__name__, *args, **kwargs)
-        return func
-
-    if callable(arg):
-        name = arg.__name__
-        return decorator(arg)
-
-    return decorator
+from typing import Optional
 
 
-@listen("v")
-def a():
-    ...
+def dcToMcFormatting(s: str) -> str:
+    legal = ["*", "_", "~"]
+    maxLen: int = len(s) - 1
+
+    result = ""
+    check: bool = False
+
+    asteriskItalic = False  # * 斜體 §o
+    underlineItalic = False  # _ 斜體 §o
+    asteriskBold = False  # **  粗體 §l
+    underlineBold = False  # __  底線 §n
+    strikeThrough = False  # ~~  刪除線 §m
+
+    def checkReset() -> str:
+        if (
+            asteriskItalic
+            and asteriskBold
+            and underlineItalic
+            and underlineBold
+            and strikeThrough
+        ):
+            return ""
+        return "§r"
+
+    index = 0
+
+    while index <= maxLen:
+        no = s[index]
+        ne: Optional[str] = s[index + 1] if maxLen > index else None  # next
+        index += 1
+
+        if no == "\\":
+            index += 1
+            result += ne or ""
+            continue
+
+        if no == "*" and ne == "*":  # - '**'
+            index += 1
+            result += "§l"
+            continue
+        if no == "_" and ne == "_":  # - '__'
+            index += 1
+            result += "§n"
+            continue
+        if no == "~" and ne == "~":  # - '~~'
+            index += 1
+            result += "§m"
+            continue
+        if no == "_" or no == "*":  # - '_' or '*'
+            result += "§o"
+            continue
+
+        result += no
+
+    return result
+
+
+print(dcToMcFormatting("0123456**awa"))
