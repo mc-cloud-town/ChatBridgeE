0eae2b2e954698adef53dbaf5483a0a466aa08bb
猴子 <a102009102009@gmail.com>
2022-11-11 21:33:30 +0800

    Update utils.py

--- chatbridgee/utils/utils.py
+++ chatbridgee/utils/utils.py
@@ -1,8 +1,7 @@
-import json
-from typing import Any, Optional
+from typing import Any
 
 
-__all__ = ("MISSING", "ClassJson")
+__all__ = ("MISSING",)
 
 
 class _MissingSentinel:
@@ -19,15 +18,15 @@ class _MissingSentinel:
 MISSING: Any = _MissingSentinel()
 
 
-class ClassJson:
-    def __init__(self, **kwargs) -> None:
-        self.__kwargs = kwargs
+# class ClassJson:
+#     def __init__(self, **kwargs) -> None:
+#         self.__kwargs = kwargs
 
-    def __repr__(self) -> str:
-        return json.dumps(self.__kwargs)
+#     def __repr__(self) -> str:
+#         return json.dumps(self.__kwargs)
 
-    def __str__(self) -> str:
-        return self.__repr__()
+#     def __str__(self) -> str:
+#         return self.__repr__()
 
-    def __getitem__(self, key: str) -> Optional[Any]:
-        return self.__kwargs.get(key, None)
+#     def __getitem__(self, key: str) -> Optional[Any]:
+#         return self.__kwargs.get(key, None)
