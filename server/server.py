4fbfc40a8a55ac49f08942a7364d6a418b23e9e0
猴貓 <a102009102009@gmail.com>
2023-01-13 21:21:14 +0800

    Update server.py

--- server/server.py
+++ server/server.py
@@ -1,4 +1,6 @@
 import asyncio
+import sys
+import traceback
 from typing import Any, Callable, Coroutine, TypeVar
 from socketio import AsyncServer
 
@@ -11,6 +13,8 @@ CoroFuncT = TypeVar("CoroFuncT", bound=CoroFunc)
 
 class Server(AsyncServer, PluginMixin):
     def __init__(self):
+        super().__init__()
+
         self.extra_events: dict[str, list[CoroFunc]] = {}
 
     def add_listener(self, func: CoroFunc, name: str = MISSING) -> None:
@@ -39,3 +43,50 @@ class Server(AsyncServer, PluginMixin):
             return func
 
         return decorator
+
+    def dispatch(self, event_name: str, *args: Any, **kwargs: Any) -> None:
+        method = f"on_{event_name}"
+
+        try:
+            coro = getattr(self, method)
+        except AttributeError:
+            pass
+        else:
+            self._schedule_event(coro, method, *args, **kwargs)
+
+        if event_name in self.extra_events:
+            for func in self.extra_events.get(method, []):
+                self._schedule_event(func, event_name, *args, **kwargs)
+
+    async def _run_event(
+        self,
+        coro: Callable[..., Coroutine[Any, Any, Any]],
+        event_name: str,
+        *args: Any,
+        **kwargs: Any,
+    ) -> None:
+        try:
+            await coro(event_name, *args, **kwargs)
+        except Exception:
+            try:
+                await self.on_error(event_name, *args, **kwargs)
+            except asyncio.CancelledError:
+                pass
+
+    def _schedule_event(
+        self,
+        coro: Callable[..., Coroutine[Any, Any, Any]],
+        event_name: str,
+        *args: Any,
+        **kwargs: Any,
+    ):
+        return asyncio.create_task(
+            self._run_event(coro, event_name, *args, **kwargs),
+            name=f"ChatBridgeE: {event_name}",
+        )
+
+    # ----- `on_` events -----
+
+    async def on_error(self, event_method: str, *args: Any, **kwargs: Any) -> None:
+        print(f"Ignoring exception in {event_method}", file=sys.stderr)
+        traceback.print_exc()
