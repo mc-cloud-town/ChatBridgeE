83d7983033835e22994c0593e7e0b249c5d67c1f
猴貓 <a102009102009@gmail.com>
2023-01-20 20:37:01 +0800

    remove old

--- client.py
+++ client.py
@@ -1,10 +1,9 @@
 from typing import Union
-import socketio
 
+import socketio
 from mcdreforged.api.all import Info, PluginServerInterface, new_thread
 
-sio: socketio.Client = None
-
+sio = socketio.Client()
 
 @new_thread("chatbridge-send-data")
 def send_event(event: str, data: Union[str, dict, list]):
@@ -22,24 +21,27 @@ def disconnect():
     print("disconnected from server")
 
 
+@new_thread("chatbridge-start")
 def on_load(server: PluginServerInterface, old_module):
-    ...
+    sio.connect("http://localhost:8080")
+    sio.wait()
 
 
+@new_thread("chatbridge-disconnect")
 def on_unload(server: PluginServerInterface):
-    ...
+    sio.disconnect()
 
 
 def on_server_start(server: PluginServerInterface):
-    ...
+    send_event("server_start")
 
 
 def on_server_startup(server: PluginServerInterface):
-    ...
+    send_event("server_startup")
 
 
 def on_server_stop(server: PluginServerInterface, return_code: int):
-    ...
+    send_event("server_stop")
 
 
 def on_info(server: PluginServerInterface, info: Info):
@@ -47,12 +49,8 @@ def on_info(server: PluginServerInterface, info: Info):
 
 
 def on_player_joined(server: PluginServerInterface, player_name: str, info: Info):
-    ...
+    send_event("player_joined", player_name)
 
 
 def on_player_left(server: PluginServerInterface, player_name: str):
-    ...
-
-
-sio.connect("http://localhost:5000")
-sio.wait()
+    send_event("player_left", player_name)
