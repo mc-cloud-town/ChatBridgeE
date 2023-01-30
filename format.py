318f4f2af1e7aee05641fcfece779fd5b599e62a
猴貓 <a102009102009@gmail.com>
2023-01-30 11:37:14 +0800

    Minecraft Chat Formatting

--- format.py
+++ format.py
@@ -1,42 +1,56 @@
+"""
+Minecraft Chat Formatting
+=========================
+This module provides a way to parse Minecraft chat formatting and colors.
+It also provides a way to convert Minecraft chat formatting and colors to
+ANSI escape sequences.
+
+reference:
+https://github.com/gnembon/fabric-carpet/blob/master/docs/scarpet/Full.md#formatcomponents--formatcomponents-
+"""
 from enum import Enum
 from typing import Optional
 
 
 class ChatFormatting(Enum):
-    OBFUSCATED = ("k", "o")
-    BOLD = ("l", "b")
-    STRIKETHROUGH = ("m", "s")
-    UNDERLINE = ("n", "u")
-    ITALIC = ("o", "i")
-    RESET = ("r")
+    # https://minecraft.fandom.com/wiki/Raw_JSON_text_format
+    OBFUSCATED = ("k", "o", None, "8")
+    BOLD = ("l", "b", None, "8")
+    STRIKETHROUGH = ("m", "s", None, "8")
+    UNDERLINE = ("n", "u", None, "8")
+    ITALIC = ("o", "i", None, "8")
 
-    BLACK = ("0", "k", 0x000000)
-    DARK_BLUE = ("1", "v", 0x0000AA)
-    DARK_GREEN = ("2", "e", 0x00AA00)
-    DARK_AQUA = ("3", "q", 0x00AAAA)
-    DARK_RED = ("4", "n", 0xAA0000)
-    DARK_PURPLE = ("5", "p", 0xAA00AA)
-    GOLD = ("6", "d", 0xFFAA00)
-    GRAY = ("7", "g", 0xAAAAAA)
-    DARK_GRAY = ("8", "f", 0x555555)
-    BLUE = ("9", "t", 0x5555FF)
-    GREEN = ("a", "l", 0x55FF55)
-    AQUA = ("b", "c", 0x55FFFF)
-    RED = ("c", "r", 0xFF5555)
-    LIGHT_PURPLE = ("d", "m", 0xFF55FF)
-    YELLOW = ("e", "y", 0xFFFF55)
-    WHITE = ("f", "w", 0xFFFFFF)
+    BLACK = ("0", "k", 0x000000, "0-30")
+    DARK_BLUE = ("1", "v", 0x0000AA, "0-34")
+    DARK_GREEN = ("2", "e", 0x00AA00, "0-32")
+    DARK_AQUA = ("3", "q", 0x00AAAA, "0-36")
+    DARK_RED = ("4", "n", 0xAA0000, "0-31")
+    DARK_PURPLE = ("5", "p", 0xAA00AA, "0-35")
+    GOLD = ("6", "d", 0xFFAA00, "0-33")
+    GRAY = ("7", "g", 0xAAAAAA, "0-37")
+    DARK_GRAY = ("8", "f", 0x555555, "0-90")
+    BLUE = ("9", "t", 0x5555FF, "0-94")
+    GREEN = ("a", "l", 0x55FF55, "0-92")
+    AQUA = ("b", "c", 0x55FFFF, "0-96")
+    RED = ("c", "r", 0xFF5555, "0-91")
+    LIGHT_PURPLE = ("d", "m", 0xFF55FF, "0-95")
+    YELLOW = ("e", "y", 0xFFFF55, "0-93")
+    WHITE = ("f", "w", 0xFFFFFF, "0-97")
 
     def __init__(
         self,
         code: str,
         mark: Optional[str] = None,
         integer: Optional[int] = None,
+        ansi: Optional[str] = None,
     ):
         self.code = code
         self.mark = mark
         self.color = -1 if integer is None else integer
         self.is_format = integer is None
+        self.is_color = not self.is_format
+
+        self.ansi = f"\033[{';'.join(ansi.split('-'))}m" if ansi else ""
 
     def __str__(self):
         return f"\u00a7{self.code}"  # f"§{self.code}"
@@ -44,20 +58,68 @@ class ChatFormatting(Enum):
     def __repr__(self):
         return f"<ChatFormatting name={self.name} mark={self.mark}>"
 
+    @classmethod
+    def formats(cls):
+        return [f for f in cls if f.is_format]
 
-class MCMessenger:
-    def get_chat_component_form_text(self, msg: str) -> dict:
-        if msg.startswith(" "):
-            msg = f"w{msg}"
-
-        desc, *texts = msg.split(" ")
-        text = " ".join(texts)
-
-    def parse_style(
-        self,
-    ):
-        ...
+    @classmethod
+    def colors(cls):
+        return [f for f in cls if not f.is_format]
 
 
-print(ChatFormatting.OBFUSCATED.is_format)
-print(ChatFormatting.OBFUSCATED.color)
+def split_desc_text(msg: str) -> tuple[str, str]:
+    if msg.startswith(" "):
+        msg = f"w{msg}"
+
+    desc, *texts = msg.split(" ")
+    return desc, " ".join(texts)
+
+
+def get_chat_component_form_text(msg: str) -> dict:
+    desc, text = split_desc_text(msg)
+    style = {"text": text}
+
+    for mark, event in {
+        "?": {"clickEvent": {"action": "suggest_command"}},
+        "!": {"clickEvent": {"action": "run_command"}},
+        "^": {"hoverEvent": {"action": "show_text"}},
+        "@": {"clickEvent": {"action": "open_url"}},
+        "&": {"clickEvent": {"action": "copy_to_clipboard"}},
+    }.items():
+        if desc.startswith(mark):
+            desc = desc[1:]
+
+            event[list(event.keys())[0]]["value"] = text
+            style.update(event)
+
+            break
+
+    return {**style, **parse_style(desc)[0]}
+
+
+def get_ansi_console(msg: str) -> tuple[dict, str]:
+    if msg.startswith(" "):
+        msg = f"w{msg}"
+
+    desc, *texts = msg.split(" ")
+    text = " ".join(texts)
+
+    return f"{parse_style(desc)[1]}{text}\033[0m"
+
+
+def parse_style(desc: str) -> tuple[dict, str]:
+    style = {}
+    ansi_str = ""
+
+    for format in ChatFormatting.formats():
+        if format.mark in desc:
+            style.update({format.name.lower(): True})
+            ansi_str += format.ansi
+
+    for color in ChatFormatting.colors():
+        if color.mark in desc:
+            style.update({"color": color.name.lower()})
+            ansi_str += color.ansi
+            break
+
+    return style, ansi_str
