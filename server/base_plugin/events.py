231bfe1e46471302ff43608c03cf5d195c075824
猴貓 <a102009102009@gmail.com>
2023-05-03 17:42:35 +0800

    Update events.py

--- server/base_plugin/events.py
+++ server/base_plugin/events.py
@@ -13,7 +13,7 @@ class BasePlugin_Events(BasePlugin, description="事件日誌"):
 
     @Plugin.listener()
     async def on_message(self, ctx: Context, msg: str):
-        log.info(f"{ctx.sid} 發送了訊息: {msg}")
+        log.info(f"[{ctx.display_name}] {ctx.sid} 發送了訊息: {msg}")
 
     @Plugin.listener()
     async def on_disconnect(self, ctx: Context):
